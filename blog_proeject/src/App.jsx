c6be11ec94be00002be90dc6a572edfbc0e090eb
ashutosh786kr <ashuronnie1234@gmail.com>
2024-06-30 18:05:49 +0530

    new imput designed and wiht forward ref learning and always use button is designed

--- blog_proeject/src/App.jsx
+++ blog_proeject/src/App.jsx
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import authService from "./appwrite/auth";
 import { login, logout } from "./store/authSlice";
 import { Header, Footer } from "./components/Index";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [loading, setLoading] = useState(true);
--- blog_proeject/src/components/Header/Header.jsx
+++ blog_proeject/src/components/Header/Header.jsx
@@ -1,7 +1,78 @@
 import React from "react";
+import { Container, Logo, LogoutBtn } from "../Index";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
-  return <div>Header</div>;
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+
+  //Array of buttons in object form  use for navigation used in producrtion grade app
+  const navItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ];
+
+  return (
+    <header className="py-3 shadow bg-slate-950">
+      <Container>
+        <nav className="flex">
+          <div className="mr-4">
+            <Link to="/">
+              <Logo width="70px" />
+            </Link>
+          </div>
+          <ul className="flex ml-auto">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full">
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {/* the below method of represnting the the logout button , if the user in logged in then 
+we will show him the logout button right ,,
+so this syntax represents that if the authStatus is true means if the user is logged in then show that user 
+that paranthesis wala part ie., logout
+*/}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  );
 }
 
 export default Header;
