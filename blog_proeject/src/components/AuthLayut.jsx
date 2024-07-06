9383be983189866fd0e6c71c3545ac4a3f2aa0fc
ashutosh786kr <ashuronnie1234@gmail.com>
2024-07-07 03:38:26 +0530

    made the Authlayout to protect the components

--- blog_proeject/src/components/AuthLayut.jsx
+++ blog_proeject/src/components/AuthLayut.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-function AuthLayut() {
-  return <div>AuthLayut</div>;
+export default function Protected({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  ///we will ask store about that uise is logged in or not amd selector has state which will telll that the user is
+  //and that state will be enough to telll that the use is logged in or not
+  const authStatus = useSelector((state) => state.auth.status);
+
+  //we will use useeffect and useEffect will tell that where to send the use to login or any place like home
+  // and changes in which places would be checkable or not
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoading(false);
+  }, [authStatus, navigate, authentication]);
+  return loading ? <h1>Loading....</h1> : <>{children}</>;
 }
-
-export default AuthLayut;
